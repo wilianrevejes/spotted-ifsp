88d0e9659eddf9e5c136bb4dd4d8496dcd1525f3
Bruno Sartori <brunosartori.webmaster@gmail.com>
2017-10-10 20:25:31 -0300

    initial commit

---
+++ Spotted/MainActivity.cs
@@ -0,0 +1,61 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.Design.Widget;
+using Android.Support.V4.Widget;
+using Android.Support.V7.Widget;
+using Android.Support.V7.App;
+
+namespace Spotted {
+    [Activity(Label = "@string/app_name", ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait, MainLauncher=true)]
+    public class MainActivity : AppCompatActivity {
+        DrawerLayout drawerLayout;
+        protected override void OnCreate(Bundle savedInstanceState) {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Main);
+            drawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+
+
+            // Initialize toolbar
+            var toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.app_bar);
+            SetSupportActionBar(toolbar);
+            SupportActionBar.SetTitle(Resource.String.app_name);
+            SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+            SupportActionBar.SetDisplayShowHomeEnabled(true);
+
+            // Attach item selected handler to navigation view
+            var navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+            navigationView.NavigationItemSelected += NavigationView_NavigationItemSelected;
+
+            // Create ActionBarDrawerToggle button and add it to the toolbar
+            var drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, Resource.String.open_drawer, Resource.String.close_drawer);
+#pragma warning disable CS0618 // O tipo ou membro é obsoleto
+            drawerLayout.SetDrawerListener(listener: drawerToggle);
+#pragma warning restore CS0618 // O tipo ou membro é obsoleto
+            drawerToggle.SyncState();
+
+            //Load default screen
+            var ft = FragmentManager.BeginTransaction();
+            ft.AddToBackStack(null);
+            ft.Add(Resource.Id.HomeFrameLayout, new HomeFragment());
+            ft.Commit();
+
+        }
+        void NavigationView_NavigationItemSelected(object sender, NavigationView.NavigationItemSelectedEventArgs e) {
+            switch (e.MenuItem.ItemId) {
+                case (Resource.Id.nav_home):
+                    // React on 'nav_home' selection
+                    break;
+                case (Resource.Id.nav_messages):
+                    //
+                    break;
+                case (Resource.Id.nav_friends):
+                    // React on 'Friends' selection
+                    break;
+            }
+            // Close drawer
+            drawerLayout.CloseDrawers();
+        }
+    }
+}
